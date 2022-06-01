154e88e3e0f322034b2f938f877924ea9de587b6
HeyChriss <christianmijangos.cm@gmail.com>
2022-06-01 11:30:38 -0600

    Creating new branch

--- WhileMathKotlinConversion/src/main/kotlin/Main.kt
+++ WhileMathKotlinConversion/src/main/kotlin/Main.kt
@@ -5,6 +5,22 @@ fun main() {
 
     fun doMath() {
 
+        fun main(args : Array<String>) {
+            print("Enter text: ")
+            var input = readLine()
+            print("You entered: $input")
+        }
+
+        fun main() {
+            val str = "A:B:C"
+            val delim = ":"
+
+            val list = str.split(delim)
+
+            println(list)    // [A, B, C]
+        }
+
+
     }
 
 
